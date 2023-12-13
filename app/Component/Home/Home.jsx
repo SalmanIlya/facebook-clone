79ec0e3a6f816cb71c7c9059379c11c303786b06
salamanilyas <salamanilyas101@gmail.com>
2023-12-13 16:15:17 +0500

    add-box-3

--- app/Component/Home/Home.jsx
+++ app/Component/Home/Home.jsx
@@ -142,7 +142,103 @@ const Home = () => {
 
 
           </div>
-          <div className="box3 w-96"></div>
+          <div className="box3 w-96">
+            <h1 className='mt-5 font-bold text-gray-400 text-sm mb-3'>Find Friends</h1>
+            <div>
+              {/* add friends  */}
+              
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+              <div className='my-3 h-14 w-72 bg-white flex flex-row items-center rounded-md  '>
+
+                <Image src={"/image/profile.jpg"} height={30} width={30} className='rounded-full w-8 h-8 mx-2 '/>
+
+                <h1 className='text-sm'>user Name</h1>
+
+                <button className='ml-14 bg-blue-500 p-2 text-white rounded text-sm'>Add Friend</button>
+              </div>
+
+            </div>
+          </div>
         </div>
         
       </div>
