2e7029834274eed4538508ed8c40096a1571c1e8
salamanilyas <salamanilyas101@gmail.com>
2023-12-13 15:38:36 +0500

    new-page

---
+++ app/FriendRequest/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import Navbar from '../Component/Navbar/Navbar'
+
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className='h-screen bg-gray-100'></div>
+    </div>
+  )
+}
+
+export default page
