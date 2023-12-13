80fa0d75ccb74063e6fe8b5b9fe8e47ccbc675a1
salamanilyas <salamanilyas101@gmail.com>
2023-12-13 15:36:48 +0500

    create-side-bar

--- app/Component/Home/Home.jsx
+++ app/Component/Home/Home.jsx
@@ -6,15 +6,75 @@ import Image from 'next/image'
 import { RiVideoAddFill } from "react-icons/ri";
 import { MdPhotoLibrary } from "react-icons/md";
 import { CgSmileMouthOpen } from "react-icons/cg";
-import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { HiOutlineDotsHorizontal, HiUserGroup } from "react-icons/hi";
+import {  MdOutlineWatchLater } from "react-icons/md";
+import { IoBookmarkSharp } from "react-icons/io5";
+import { FaUserGroup } from "react-icons/fa6";
+import { PiVideoFill } from "react-icons/pi";
+import { FaShop } from "react-icons/fa6";
+import { FaRegNewspaper } from "react-icons/fa";
+import { IoIosArrowDown } from "react-icons/io";
+
 const Home = () => {
   return (
     <div className=''>
       <Navbar/>
       <div className='bg-gray-100 min-h-screen max-h-auto '>
-        <div className='flex flex-row justify-center px-20'>
-          <div className='box1 w-64'></div>
-          <div className="box2">
+        <div className='flex flex-row justify-center '>
+          {/* box-1  */}
+          <div className='box1 w-64 flex flex-col'>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <Image src={"/image/profile.jpg"} className="rounded-full" height={20} width={30}/>
+<p className='ml-2'>Salaman ilyas</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <FaUserGroup className='text-blue-500 text-2xl'/>
+<p className='ml-2'>Friends</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <MdOutlineWatchLater className='text-green-400 text-2xl'/>
+<p className='ml-2'>Memories</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+<IoBookmarkSharp className='text-2xl text-purple-400 '/>
+<p className='ml-2'>Save</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <HiUserGroup className='text-2xl text-blue-400 ' />
+<p className='ml-2'>Groups</p>
+            </div>
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <PiVideoFill className='text-2xl text-blue-400 ' />
+<p className='ml-2'>Videos</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <FaShop className='text-2xl text-blue-700 ' />
+<p className='ml-2'>MarketPlace</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2 '>
+            <FaRegNewspaper className='text-2xl text-blue-700 ' />
+<p className='ml-2'>Feeds</p>
+            </div>
+
+            <div className='flex flex-row justify-start mt-5 ml-2'>
+            <IoIosArrowDown className='text-2xl text-gray-700  bg-gray-300 p-1 rounded-full ' />
+<p className='ml-2'>See More</p>
+            </div>
+            <hr className='mt-3' />
+            <hr />
+            <div className='' ></div>
+
+
+          </div>
+          {/* box-2  */}
+          <div className="box2 mx-14">
             <br />
         
           <div className="postdiv p-2 pt-3">
@@ -53,8 +113,31 @@ const Home = () => {
 
 </div>
 <br />
-<br />
+<div className='post flex flex-col justify-between '>
+  <div className='flex flex-row justify-between items-center py-4'>
+  <div className='flex flex-row items-center pt-3 pl-2' >
+<Image src={"/image/profile.jpg"} className='profile rounded-full mx-1' height={30} width={30} />
+<p className='text-gray-500 text-sm ml-2 font-bold'>salaman Ilyas</p>
+</div>
+<HiOutlineDotsHorizontal className='mr-3 mt-2'/>
+  </div>
+ 
+<Image src={"/image/3.jpg"} height={1} width={200} className=' w-full  rounded-b-xl   '/>
 
+</div>
+<br />
+<div className='post flex flex-col justify-between '>
+  <div className='flex flex-row justify-between items-center py-4'>
+  <div className='flex flex-row items-center pt-3 pl-2' >
+<Image src={"/image/profile.jpg"} className='profile rounded-full mx-1' height={30} width={30} />
+<p className='text-gray-500 text-sm ml-2 font-bold'>salaman Ilyas</p>
+</div>
+<HiOutlineDotsHorizontal className='mr-3 mt-2'/>
+  </div>
+ 
+<Image src={"/image/4.jpg"} height={1} width={200} className=' w-full  rounded-b-xl   '/>
+
+</div>
 
 
 
