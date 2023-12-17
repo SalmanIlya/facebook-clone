1f7971d067989f40436f5d0dc315a6a3a37c7de0
salamanilyas <salamanilyas101@gmail.com>
2023-12-17 18:33:22 +0500

    add-redux-toolkit-in-layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,24 @@
+"use client"
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Provider } from 'react-redux'
+import { store } from './Store/Store'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Facebook Clone',
-  description: 'create clone with next.js and node.js',
-}
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+     <Provider store={store}>
+     {children}
+
+     </Provider>
+
+        
+        </body>
     </html>
   )
 }
