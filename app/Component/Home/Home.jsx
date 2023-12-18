a6608cce42170fd76e816aeec29e87a6dfe60660
salamanilyas <salamanilyas101@gmail.com>
2023-12-18 12:39:44 +0500

    small-update

--- app/Component/Home/Home.jsx
+++ app/Component/Home/Home.jsx
@@ -6,10 +6,21 @@ import Box1 from './Box1'
 import Box2 from './Box2'
 import Box3 from './Box3'
 import { useSelector } from 'react-redux'
+import Link from 'next/link'
 
 const Home = () => {
-  const User=useSelector((state)=>state.User.user)
-  console.log("User : ",User);
+  // const User=useSelector((state)=>state.User.user)
+  const User=useSelector((State)=>State.User.user)
+  console.log(User);
+  if(User.email===undefined){
+    return <div className='flex flex-col justify-center items-center h-screen text-2xl '>
+    <p className='font-bold'>Please Login First</p>
+    <Link className='bg-blue-500 hover:bg-blue-700 border-none p-2 text-white mt-3 rounded-lg' href={"/Login"}>Login</Link>
+    <p>or</p>
+    <Link className='border-none bg-blue-500 p-2 text-white rounded-lg hover:bg-blue-700' href={"/Register"}>Register</Link>
+  </div>
+  }else{
+
   return (
     <div className=''>
       <Navbar/>
@@ -35,7 +46,7 @@ const Home = () => {
         
       </div>
     </div>
-  )
+  )}
 }
 
 export default Home
