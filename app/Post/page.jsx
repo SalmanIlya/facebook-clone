db030d7069fa3fd288f88f394c98dd2b0aeaa5af
salamanilyas <salamanilyas101@gmail.com>
2023-12-19 18:44:45 +0500

    make-post-page-responsive

--- app/Post/page.jsx
+++ app/Post/page.jsx
@@ -8,6 +8,7 @@ import Link from 'next/link';
 
 const page = () => {
 const User=useSelector((state)=>state.User.user)
+
 if(User.email===undefined){
   return <div className='flex flex-col justify-center items-center h-screen text-2xl '>
     <p className='font-bold'>Please Login First</p>
@@ -31,10 +32,10 @@ if(User.email===undefined){
 }  className='rounded-full m-5' height={50} width={50}/>
 <p>{User.username}</p>
             </div>
-            <textarea name="" id="" cols="30" rows="10" placeholder='Post a status upload '  className=' rounded-3xl outline-none bg-gray-200 w-96 p-3 ' ></textarea>
-           <div className='relative my-4 cursor-pointer'>
+            <textarea name="" id="" cols="30" rows="10" placeholder='Post a status upload '  className=' rounded-3xl outline-none bg-gray-200 w-80 sm:w-96 md:w-96 lg:w-96 2xl:w-1/3   p-3 ' ></textarea>
+           <div className='relative my-4 '>
            
-           <input type="file" name="" id=""  className='absolute opacity-0 z-20 w-40'  />
+           <input type="file" name="" id=""  className='absolute cursor-pointer opacity-0 left-0 top-4 z-20 w-40 h-40 '  />
            <div className='absolute flex flex-row items-center mt-3 z-0 w-40' >
 
 <MdOutlinePhotoLibrary className='text-green-500 text-4xl'/>
