de0174b9e6dc8547cb5a3a0802cd5510d2d96bdc
salamanilyas <salamanilyas101@gmail.com>
2023-12-19 18:45:25 +0500

    small-update

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -14,26 +14,31 @@ if(User.email!==undefined){
         <Navbar/>
         <div className='max-h-auto min-h-screen bg-gray-100'>
           <div className="images flex flex-col items-center relative ">
-            <Image alt='' src={User.coverPhoto==="" ? "/image/profile.jpg":User.coverPhoto} height={300} width={1000} className='h-96  '/>
-            <Image alt='' src={User.profilePhoto==="" ? "/image/profile.jpg":User.profilePhoto} height={250} width={250} className='rounded-full h-64 absolute border-2 border-white top-60 left-60' />
-           <div className='flex flex-col items-center'>
-           <p className='text-3xl mt-5 font-bold text-gray-800 '>{User.username} </p>
+            <Image alt='' src={User.coverPhoto==="" ? "/image/profile.jpg":User.coverPhoto} height={0} width={0} className='h-64 md:h-72 lg:h-80 xl:h-96 2xl:h-96  w-full '/>
+            <Image alt='' src={
+              User.profilePhoto==="" ?
+               "/image/profile.jpg"
+               :User.profilePhoto
+               } height={0} width={0} className='rounded-full h-36 w-36 sm:w-52 sm:h-52 md:w-60 md:h-60 lg:w-60 lg:h-60 absolute border-2 border-white top-40 left-5 2xl:left-60 md:left-20 lg:left-40   ' />
+           <div className='flex flex-col sm:items-end md:items-center lg:items-center xl:items-center 2xl:items-center mr-40 items-end    w-full  mt-14'>
+           <p className='text-3xl mt-5 font-bold text-gray-800 '>{User.username} hello </p>
             <p  className="font-light text-gray-400  ">{User.followers.length===undefined ? 10:User.followers.length }  friends </p>
-          <button className='bg-blue-500 text-white px-3 py-2 rounded-lg mt-2'>Edit</button>
+          <button className='hover:bg-blue-700 bg-blue-500 text-white px-3 py-2 rounded-lg mt-2 w-20'>Edit</button>
+          <button className='bg-blue-500 text-white px-3 py-2 rounded-lg mt-2 w-20 hover:bg-blue-700 '>LogOut</button>
            </div>
             
           </div>
           <div className='flex flex-col justify-center items-center mt-36 pt-10 '>
             <p className=' mb-5 text-gray-500 text-2xl font-light'>Friends ({User.followers.length})</p>
             <div className='mx-20 h-1/2 w-1/2 flex flex-row justify-center items-center flex-wrap '>
-              {
-                User.followers.length===0?
+               {
+                 User.followers.length===0?
                 <></>
                 :
                 User.followers.map((item)=>{
 return <Image className='m-1 cursor-pointer rounded-xl' alt='' src={item.profilePhoto} height={200} width={200} />
                 })
-              }
+              } 
           </div>
           </div>
 
