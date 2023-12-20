a1d3a33b15f99cf1e7d8139db79d4a5c7ea29d22
salamanilyas <salamanilyas101@gmail.com>
2023-12-20 20:20:21 +0500

    Update Store.js

--- app/Store/Store.js
+++ app/Store/Store.js
@@ -1,8 +1,12 @@
 "use client"
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from '@reduxjs/toolkit';
 import User from "./User";
+
 export const store=configureStore({
+    
     reducer:{
-User:User
+User:User,
+
     }
 })
